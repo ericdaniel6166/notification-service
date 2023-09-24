cc11a1bb612394ca30065c679e6199c620f21f0b
eric6166 <ericnguyen6166@gmail.com>
2023-09-24 21:19:59 +0700

    feature/SM-0023

---
+++ src/main/java/com/example/notificationservice/kafka/KafkaConsumerProperties.java
@@ -0,0 +1,23 @@
+package com.example.notificationservice.kafka;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@ConditionalOnProperty(prefix = "spring.kafka", name = "enabled")
+public class KafkaConsumerProperties {
+
+    @Value("${spring.kafka.consumers.internal-consumer.group-id}")
+    String internalGroupId;
+
+    @Value("${spring.kafka.consumers.notification-consumer.group-id}")
+    String notificationGroupId;
+
+
+}
