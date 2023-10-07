b010f586fda4d0828dafada7880817c1bd17dde0
eric6166 <ericnguyen6166@gmail.com>
2023-10-07 18:37:26 +0700

    feature/SM-0028

--- src/main/java/com/example/notificationservice/config/kafka/KafkaConsumerGroupIdConfig.java
+++ src/main/java/com/example/notificationservice/config/kafka/KafkaConsumerGroupIdConfig.java
@@ -1,6 +1,6 @@
-package com.example.notificationservice.kafka;
+package com.example.notificationservice.config.kafka;
 
-import com.example.springbootmicroservicesframework.kafka.KafkaConsumerConfig;
+import com.example.springbootmicroservicesframework.config.kafka.KafkaConsumerConfig;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
