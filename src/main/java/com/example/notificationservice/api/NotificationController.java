9951ae7f19c0ec9e876d79724b6f1a28757185c5
eric6166 <ericnguyen6166@gmail.com>
2023-10-15 19:56:22 +0700

    feature/SM-0032

--- src/main/java/com/example/notificationservice/api/NotificationController.java
+++ src/main/java/com/example/notificationservice/api/NotificationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/notification")
+@RequestMapping("/")
 @RequiredArgsConstructor
 @Slf4j
 public class NotificationController {
