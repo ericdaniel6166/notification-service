b16820de3f47a61fbe887f94081ad5b7b794e2ef
eric6166 <ericnguyen6166@gmail.com>
2023-10-17 00:56:13 +0700

    feature/SM-0032

--- src/main/java/com/example/notificationservice/api/NotificationApi.java
+++ src/main/java/com/example/notificationservice/api/NotificationApi.java
@@ -4,20 +4,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 @RequiredArgsConstructor
 @Slf4j
-public class NotificationController {
+public class NotificationApi {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public String getSampleNotification() {
-        log.info("Get sample notification");
-        return "Sample notification";
+    public String test() {
+        log.info("test");
+        return "test";
     }
 }
