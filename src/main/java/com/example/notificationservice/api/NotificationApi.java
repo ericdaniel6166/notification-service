8d5f3740bf82ae1a111d5e838011518b8cfa2100
ericdaniel6166 <64393957+ericdaniel6166@users.noreply.github.com>
2023-10-23 21:28:34 +0700

    feature/SM-0035 (#14)

--- src/main/java/com/example/notificationservice/api/NotificationApi.java
+++ src/main/java/com/example/notificationservice/api/NotificationApi.java
@@ -2,9 +2,8 @@ package com.example.notificationservice.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class NotificationApi {
 
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public String test() {
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
         log.info("test");
-        return "test";
+        return ResponseEntity.ok("test");
     }
 }
